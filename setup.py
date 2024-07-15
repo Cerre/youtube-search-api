c4b2495c1896894a1be35d419a9fa562364dc35d
Filip Cederquist <cederquist94@hotmail.com>
2024-07-15 16:17:15 +0200

    update packages and setup file

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="youtube-search-api",
+    version="0.1.0",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        fastapi,
+        uvicorn,
+        pinecone-client,
+        openai,
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+        ]
+    },
+    author="Filip Ccederquist",
+    author_email="cederquist94@hotmail.com",
+    description="A way to search transcripts from youtube videos",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/cerre/youtube-search-api/",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
